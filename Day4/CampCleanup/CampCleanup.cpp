e27ae7a10d176a3d1578b354c04fdecbc09633cf
Barnaby Dellar <barnaby.dellar+cmi@mre.medical.canon>
2022-12-04 10:07:56 +0000

    Day4

--- Day4/CampCleanup/CampCleanup.cpp
+++ Day4/CampCleanup/CampCleanup.cpp
@@ -14,11 +14,20 @@ int main()
     const auto lines = FileReader::Lines("Cleanup.txt");
     std::vector<std::pair<Range, Range>> ranges;
     ranges.reserve(lines.size());
-    for (const auto& line : lines)
-    {
-        ranges.push_back(LineSplitter::Split(line));
-    }
+    std::ranges::transform(
+        lines, 
+        std::back_inserter(ranges), 
+        [](const auto& l)
+        {
+            return LineSplitter::Split(l);
+        });
 
-    const auto contained = std::ranges::count_if(ranges, [](auto p) {return RangesContain::Contains(p); });
+    const auto contained = std::ranges::count_if(
+        ranges, 
+        [](auto p)
+        {
+            return RangesContain::Contains(p);
+        });
+
     std::cout << "There were " << contained << " contained pairs";
 }
