84876fa720d5a9fa24e4bf7d200a2ea6b9c99ab1
Barnaby Dellar <barnaby.dellar+cmi@mre.medical.canon>
2022-12-16 10:31:02 +0000

    .

---
+++ Day6/Tuning/TuningTrouble.cs
@@ -0,0 +1,5 @@
+﻿namespace Tuning;
+
+public class TuningTrouble
+{
+}
---
+++ Day6/TuningTests/TuningTroubleTests.cs
@@ -0,0 +1,12 @@
+using Tuning;
+
+namespace TuningTests;
+
+public class TuningTroubleTests
+{
+    [Test]
+    public void TuningTroubleCanBeCreated()
+    {
+        var t = new TuningTrouble();
+    }
+}
